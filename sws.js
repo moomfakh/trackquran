128587a693adb9480712dd034eebe1c17d75ead0
moomfakh <89586272+moomfakh@users.noreply.github.com>
2022-03-13 12:45:05 +0530

    Update sws.js

--- sws.js
+++ sws.js
@@ -1,1 +1,39 @@
+self.addEventListener('fetch', function (event) {
 
+  event.respondWith(
+
+    caches.open('mysite-dynamic').then(function (cache) {
+
+      return cache.match(event.request).then(function (response) {
+
+        var fetchPromise = fetch(event.request).then(function (networkResponse) {
+
+          cache.put(event.request, networkResponse.clone());
+
+          return networkResponse;
+
+        });
+
+        return response || fetchPromise;
+
+      });
+
+    }),
+
+  );
+
+});
+
+self.addEventListener('fetch', function (event) {
+
+  event.respondWith(
+
+    caches.match(event.request).then(function (response) {
+
+      return response || fetch(event.request);
+
+    }),
+
+  );
+
+});
